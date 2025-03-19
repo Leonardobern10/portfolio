94e8fd2ed407670130264b5f3f2fc5c52a432480
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-03-19 00:53:58 -0300

    modified:   .gitignore 	new file:   src/constants/articles.js 	new file:   src/constants/index.js

---
+++ src/constants/articles.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const mediumProfileURL =
+  "https://mediumpostsapi.vercel.app/api/@leonardo.bernardo2658";
+
+export const scrapMedium = async () => {
+  try {
+    const { data } = await axios.get(mediumProfileURL);
+    const value = data.dataMedium;
+    console.log(value);
+    return value;
+  } catch (error) {
+    console.error(error);
+  }
+};
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const HERO_CONTENT = `Sou estudante de graduação em Engenharia de Software, com talento para criar aplicações robustas e escaláveis. Através dos meus projetos, aperfeiçoei minhas habilidades em tecnologias de front-end, mas com uma aptidão maior para tecnologias de back-end como Java, Node.js. Meu objetivo é aproveitar minha experiência, evoluindo continuamente em diversos temas, para criar soluções inovadoras que impulsionem o crescimento dos negócios e proporcionem influências significativas na vida das pessoas, tornando suas experiências mais agradáveis.`;
+
+export const ABOUT_TEXT_TITLE = {
+  D: ["d", "edicação."],
+  E: ["e", "mpatia."],
+  V: ["v", "ontade."],
+};
+export const ABOUT_TEXT_CONTENT = `Gosto de desafios e, principalmente, resolvê-los. Procuro sempre me aprofundar nos conceitos sobre tecnologias e linguagens. Minhas áreas de maior destaque são aplicações com Java, JavaScript e bancos de dados como PostgreSQL, MongoDB, CassandraDB, Neo4J e Redis; além da utilização de mensagerias, como RabbitMQ com o protocolo AMQP, para criação de microserviços. Também tenho experiência com React, TypeScript, Nodejs e Spring Framework (Spring Security, Spring Web, Spring Data JPA).`;
+
+export const CONTACT = {
+  address: "Nova Iguaçu, RJ, Brasil",
+  phoneNo: "+55 21 9 9942-9912 ",
+  email: "leonardo.bernardo2658@gmail.com",
+};
