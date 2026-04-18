be1ab652dca0e738766caf5eda05bc98cf0e2802
= <=>
2026-04-18 16:41:19 -0300

    "feat: donwload resume"

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,16 +1,21 @@
-import { MEDIUM_URL } from '../data/articlesData';
-
 type ButtonLinkProps = {
     title: string;
+    download?: boolean;
+    href: string;
 };
 
-export default function ButtonLink({ title }: ButtonLinkProps) {
+export default function ButtonLink({
+    title,
+    href,
+    download = false,
+}: ButtonLinkProps) {
     return (
         <a
-            href={MEDIUM_URL}
+            href={href}
             target="_blank"
             rel="noreferrer"
-            className="font-mono-custom text-[13px] text-[#f0ede8] border border-white/[0.15] px-8 py-3.5 rounded-sm inline-block hover:border-white transition-colors tracking-wide">
+            download={download}
+            className="font-mono-custom text-[13px] text-[#f0ede8] border border-white/15 px-8 py-3.5 rounded-sm inline-block hover:border-white transition-colors tracking-wide">
             {title}
         </a>
     );
